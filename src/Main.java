d27645360a5afd1d9b6479cd9b4fcf43ee938751
spydxn <guil2013sp@gmail.com>
2026-08-14 16:35:24 -0300

    Classe Produto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Produto> listaDeProduto = new ArrayList<>();
+
+        System.out.println("Seja Bem-vindo!");
+        System.out.println("Quantos produtos você deseja cadastrar?");
+
+        int quantidade = scanner.nextInt();  // armazenando número inteiro
+        scanner.nextLine();  //Limpeza de buffer
         }
-    }
+
+public static class Produto {
+        String produto;
+        String cor;
+        int qtde;
+        float preco;
+
+public void exibirProduto(){
+    System.out.println("Produto: " + produto + "Cor: " + cor + "Quantidade: " + qtde + "Preço: " + preco);
+
+}
+}
+
+}
+
+//realizar calculo preço * qtde
