f24e1919eee618dab4aa4c54d229c0f210a54c52
spydxn <guil2013sp@gmail.com>
2026-08-15 17:49:44 -0300

    CÃFalta mostrar os produtos cadastrados

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in); //Scannerconversa com o usuário
         ArrayList<Produto> listaDeProduto = new ArrayList<>();
 
         System.out.println("Seja Bem-vindo!");
@@ -13,10 +13,10 @@ public class Main {
         int quantidade = scanner.nextInt();  // armazenando número inteiro
         scanner.nextLine();
 
-        if(quantidade <= 0){
+        if (quantidade <= 0) {
             System.out.println("Quantidade inválida. O sistema será encerrado.");
-        }else{
-            for (int i = 0; i < quantidade; i++){
+        } else {
+            for (int i = 0; i < quantidade; i++) {
                 System.out.println("\n--- Cadastro do Produto" + (i + 1) + " ---");
 
                 Produto novoProduto = new Produto();
@@ -36,25 +36,49 @@ public class Main {
 
                 listaDeProduto.add(novoProduto);
             }
-            scanner.close();
-            System.out.println("\nSistema encerrado. Muito Obrigado!");
+
+            exibirProduto(listaDeProduto);
+        }
+        scanner.close();
+        System.out.println("\nSistema encerrado. Muito Obrigado!");
+
+    }
+
+    public static void exibirProduto(ArrayList<Produto> lista) {
+        System.out.println("=====================================");
+        System.out.println("\n   LISTA DOS PRODUTOS CADASTRADOS   ");
+        System.out.println("=====================================");
+
+        for (int i = 0; 1 < lista.size(); i++) {
+            Produto produtoAtual = lista.get(i);
+            System.out.println((i + 1) + ". ");
+            produtoAtual.exibirProduto();
+
+            if (produtoAtual.preco >= 2000) {
+                System.out.println("     -> Status: Compra minima atingida!");
+            } else {
+                System.out.println("     -> Status: Compra minima não atingida!");
+            }
         }
 
-        //Limpeza de buffer
-        }
+    }
 
-public static class Produto {
+    public static class Produto {
         String produto;
         String cor;
         int qtde;
         float preco;
 
-public void exibirProduto(){
-    System.out.println("Produto: " + produto + "Cor: " + cor + "Quantidade: " + qtde + "Preço: " + preco);
+        public void exibirProduto() {
+            System.out.println("Produto: " + produto + "Cor: " + cor + "Quantidade: " + qtde + "Preço: " + preco);
 
-}
-}
+        }
+    }
 
 }
 
 //realizar calculo preço * qtde
+//se o preço passar de 2000 reais ganha frete de 30/60/90 dias
+//representante pode escolher junto com a empresa quanto de desconto o cliente pode ganhar com o pedido
+//escolha da forma de pagamento
+
